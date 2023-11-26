694ecbf0352d2ec5fc28172a6f3edbd024295e73
ErNeRooo <erneroooooo@gmail.com>
2023-11-26 11:06:34 +0100

    add basic app ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,69 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [screenString, setOperationString] = useState("");
+  const [result, setResult] = useState("");
+
+  setResult("");
+
+  const setString = (str: string) => {
+    setOperationString((prev) => {
+      return prev + str;
+    });
+  };
+
   return (
     <>
-      <h1 className="text-3xl">ha</h1>
+      <h1>Fancy Calculator</h1>
+
+      <Screen operationString={screenString} result={result} />
+
+      <NumberButton number={0} />
+      <NumberButton number={1} />
+      <NumberButton number={2} />
+      <NumberButton number={3} />
+      <br />
+      <NumberButton number={4} />
+      <NumberButton number={5} />
+      <NumberButton number={6} />
+      <NumberButton number={7} />
+      <br />
+
+      <NumberButton number={8} />
+      <NumberButton number={9} />
+      <OperationButton
+        operationSymbol={"-"}
+        setString={setString}
+        screenString={screenString}
+      />
+      <OperationButton
+        operationSymbol={"+"}
+        setString={setString}
+        screenString={screenString}
+      />
+      <br />
+      <OperationButton
+        operationSymbol={"÷"}
+        setString={setString}
+        screenString={screenString}
+      />
+      <OperationButton
+        operationSymbol={"×"}
+        setString={setString}
+        screenString={screenString}
+      />
+      <OperationButton
+        operationSymbol={"^2"}
+        setString={setString}
+        screenString={screenString}
+      />
+      <OperationButton
+        operationSymbol={"√"}
+        setString={setString}
+        screenString={screenString}
+      />
     </>
-  );
 }
 
 export default App;
