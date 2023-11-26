566ccf2c1b9b3d9e91704148d85ac45846017f62
ErNeRooo <erneroooooo@gmail.com>
2023-11-26 16:28:36 +0100

    add operationScreen behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Button } from "./components/Button";
 import { Screen } from "./components/Screen";
 
 function App() {
-  const [operationString, setOperationString] = useState("4 + 2 + 3 + 1 / 7");
-  const [result, setResult] = useState("1.33333333333");
+  const [operationString, setOperationString] = useState("");
+  const [result, setResult] = useState("");
 
-  const setString = (symbol: string) => {
+  const add = (symbol: string) => {
+    setOperationString((prev) => prev + symbol);
+  };
+  const del = () => {
     setOperationString((prev) => {
-      return prev + symbol;
-
-      setResult("setResult trolling");
+      const str: string = prev.slice(0, -1);
+      return str;
     });
+  };
+  const clear = () => {
+    setOperationString("");
   };
 
   return (
@@ -19,35 +24,36 @@ function App() {
       <Screen operationString={operationString} result={result} />
 
       <div className="w-fit mx-auto">
-        <Button symbol={"^"} str={operationString} setString={setString} />
-        <Button symbol={"√"} str={operationString} setString={setString} />
-        <Button symbol={"Del"} str={operationString} setString={setString} />
-        <Button symbol={"C"} str={operationString} setString={setString} />
+        <Button symbol={"^"} str={operationString} setter={add} />
+        <Button symbol={"√"} str={operationString} setter={add} />
+        <Button symbol={"Del"} str={operationString} setter={del} />
+        <Button symbol={"C"} str={operationString} setter={clear} />
         <br />
 
-        <Button symbol={"1"} str={operationString} setString={setString} />
-        <Button symbol={"2"} str={operationString} setString={setString} />
-        <Button symbol={"3"} str={operationString} setString={setString} />
-        <Button symbol={"÷"} str={operationString} setString={setString} />
+        <Button symbol={"1"} str={operationString} setter={add} />
+        <Button symbol={"2"} str={operationString} setter={add} />
+        <Button symbol={"3"} str={operationString} setter={add} />
+        <Button symbol={"÷"} str={operationString} setter={add} />
         <br />
 
-        <Button symbol={"4"} str={operationString} setString={setString} />
-        <Button symbol={"5"} str={operationString} setString={setString} />
-        <Button symbol={"6"} str={operationString} setString={setString} />
-        <Button symbol={"×"} str={operationString} setString={setString} />
+        <Button symbol={"4"} str={operationString} setter={add} />
+        <Button symbol={"5"} str={operationString} setter={add} />
+        <Button symbol={"6"} str={operationString} setter={add} />
+        <Button symbol={"×"} str={operationString} setter={add} />
         <br />
-        <Button symbol={"7"} str={operationString} setString={setString} />
-        <Button symbol={"8"} str={operationString} setString={setString} />
-        <Button symbol={"9"} str={operationString} setString={setString} />
-        <Button symbol={"-"} str={operationString} setString={setString} />
+        <Button symbol={"7"} str={operationString} setter={add} />
+        <Button symbol={"8"} str={operationString} setter={add} />
+        <Button symbol={"9"} str={operationString} setter={add} />
+        <Button symbol={"-"} str={operationString} setter={add} />
         <br />
-        <Button symbol={"."} str={operationString} setString={setString} />
-        <Button symbol={"0"} str={operationString} setString={setString} />
-        <Button symbol={"="} str={operationString} setString={setString} />
-        <Button symbol={"+"} str={operationString} setString={setString} />
+        <Button symbol={"."} str={operationString} setter={add} />
+        <Button symbol={"0"} str={operationString} setter={add} />
+        <Button symbol={"="} str={operationString} setter={add} />
+        <Button symbol={"+"} str={operationString} setter={add} />
       </div>
     </div>
   );
+  setResult("");
 }
 
 export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,57 @@
-export const Button = ({ symbol, str: operation, setString }: IProps) => {
-  //dodaje stringa
-  //
-  return <button className="btn">{symbol}</button>;
+const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operationSymbols: string[] = ["^", "√", "÷", "×", "-", "+"];
+
+export const Button = ({ symbol, str, setter }: IProps) => {
+  const handleNumberClick = () => {
+    if (str.length > 20) return;
+
+    setter(symbol);
+  };
+
+  const handleOperationClick = () => {
+    if (str.length > 20) return;
+
+    if (operationSymbols.includes(str[str.length - 1])) return;
+
+    setter(symbol);
+  };
+
+  const handleSpecialClick = () => {
+    setter("");
+  };
+
+  if (numbers.toString().includes(symbol))
+    return (
+      <button onClick={handleNumberClick} className="btn bg-stone-500">
+        {symbol}
+      </button>
+    );
+  else if (["C", "Del"].includes(symbol))
+    return (
+      <button onClick={handleSpecialClick} className="btn">
+        {symbol}
+      </button>
+    );
+  else if (symbol === "=")
+    return (
+      <button onClick={handleSpecialClick} className="btn bg-blue-500">
+        {symbol}
+      </button>
+    );
+  else
+    return (
+      <button onClick={handleOperationClick} className="btn ">
+        {symbol}
+      </button>
+    );
+
   symbol;
-  setString;
-  operation;
+  setter;
+  str;
 };
 
 interface IProps {
   symbol: string;
   str: string;
-  setString: (symbol: string) => void;
+  setter: (symbol: string) => void;
 }
